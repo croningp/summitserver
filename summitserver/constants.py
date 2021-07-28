8b8e1281bdf6198db6e88e89b73398d2bf2df434
Artem Leonov <artem.leonov@glasgow.ac.uk>
2021-07-28 11:06:14 +0000

    Server update for batch-wise operation

--- summitserver/constants.py
+++ summitserver/constants.py
@@ -1,14 +1,18 @@
 """ Module constants. """
 
-# from summit.strategies.tsemo import TSEMO
+from summit.strategies.tsemo import TSEMO
 from summit.strategies.sobo import SOBO
 from summit.strategies.neldermead import NelderMead
 from summit.strategies.snobfit import SNOBFIT
+from summit.strategies.multitask import MTBO
+from summit.strategies.entmoot import ENTMOOT
 
 
 ALGORITHMS_MAPPING = {
-    # 'TSEMO': TSEMO,
+    'TSEMO': TSEMO,
     'SOBO': SOBO,
+    'MTBO': MTBO,
+    'ENTMOOT': ENTMOOT,
     'NelderMead': NelderMead,
     'SNOBFIT': SNOBFIT,
 }
